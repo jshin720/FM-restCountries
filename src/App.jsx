e2ce30e28c170526ab022b7a65c1658ed0f64821
jshin720 <jeffshin720@gmail.com>
2023-06-20 14:58:29 -0400

    console logging if the data from json is coming through properly

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState, useEffect } from 'react'
 import './App.css'
 import Header from './components/header/Header.jsx';
 import Main from './components/main/main.jsx';
 import Index from './components/index/Index.jsx'
+
+import Countries from "../public/data.json"
 
 function App() {
   const [count, setCount] = useState(0)
@@ -12,9 +12,16 @@ function App() {
   return (
     <>
       <div>
-        <Header/>
+        {
+          Countries.map(country => {
+            console.log("Country", country)
+          })
+  
+        }
+
+        {/* <Header/>
         <Main/>
-        <Index/>
+        <Index/> */}
         {/* <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
@@ -31,9 +38,9 @@ function App() {
           Edit <code>src/App.jsx</code> and save to test HMR
         </p> */}
       </div>
-      <p className="read-the-docs">
+      {/* <p className="read-the-docs">
         Click on the Vite and React logos to learn more
-      </p>
+      </p> */}
     </>
   )
 }
--- src/components/main/main.jsx
+++ src/components/main/main.jsx
@@ -1,6 +1,11 @@
 import React from 'react';
 
 function main() {
+
+  
+
+
+
   return (
     <div>
       <div>back button</div>

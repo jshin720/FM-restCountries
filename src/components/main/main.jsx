9635f53f548a0aff4986883e77d14a17f87b4a8e
jshin720 <jeffshin720@gmail.com>
2023-06-20 13:57:36 -0400

    wrote html for main component

--- src/components/main/main.jsx
+++ src/components/main/main.jsx
@@ -3,7 +3,29 @@ import React from 'react';
 function main() {
   return (
     <div>
-      
+      <div>back button</div>
+      <div>
+        <div className="img-container">
+          <h4>flag img</h4>
+        </div>
+        <div className="info-container">
+          <div>
+            <h4>country name</h4>
+            <p></p>
+            <p></p>
+            <p></p>
+            <p></p>
+            <p></p>
+          </div>
+
+          <div>
+            <p></p>
+            <p></p>
+            <p></p>
+          </div>
+        </div>
+        <div>border countries</div>
+      </div>
     </div>
   );
 }

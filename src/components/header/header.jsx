d5cde4b6d699e0f8b320b54ff7cf9d91c0a64822
jshin720 <jeffshin720@gmail.com>
2023-06-20 13:39:56 -0400

    wrote html for header component

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -3,7 +3,12 @@ import React from 'react';
 function header() {
   return (
     <div>
-      
+      <h4>Where in the world?</h4>
+      <div>
+        <span>moon img</span>
+        <h5>dark mode or lite mode</h5>
+      </div>
+
     </div>
   );
 }

314f63d8087ab36741e86eb4efb75983b00abd9f
jshin720 <jeffshin720@gmail.com>
2023-06-26 12:28:13 -0400

    working on search and filter bug

--- src/App.jsx
+++ src/App.jsx
@@ -78,23 +78,9 @@ function App() {
       </select>
 
       <div>
-        {!searchType ? (
-          countries.map((country, i) => {
-            return (
-              <div className="country-container" key={i}>
-                <img alt="flag" title={country.name} src={country.flags.svg} />
-                <span>
-                  <p>{country.name}</p>
-                  <p>Population: {country.population}</p>
-                  <p>Region: {country.region}</p>
-                  <p>Capital: {country.capital}</p>
-                </span>
-              </div>
-            );
-          })
-        ) : (
+       
           <Search keyword={keyword} filter={filter} searchType={searchType} />
-        )}
+        
       </div>
     </>
   );
--- src/components/search/Search.jsx
+++ src/components/search/Search.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Countries from "../../data.json"
+import countries from "../../data.json"
 
 function Search({keyword, filter, searchType}) {
 
@@ -7,60 +7,77 @@ function Search({keyword, filter, searchType}) {
 
   const searchResults = () => {
 
-    const sResults = Countries.filter((country) => country.name.toLowerCase().includes(keyword.toLowerCase()));
+    const sResults = countries.filter((country) => country.name.toLowerCase().includes(keyword.toLowerCase()));
     // console.log('results', results)
-
-    return (
-      <div>
-        {
-          sResults.map((country, i) => {
-            return (
-              <div className="country-container" key={i}>
-                <img src={country.flags.svg}></img>
-                <span>
-                  <p>{country.name}</p>
-                  <p>Population: {country.population}</p>
-                  <p>Region: {country.region}</p>
-                  <p>Capital: {country.capital}</p>
-                </span>
-              </div>
-            );
-          })
-        }
-      </div> 
-    );
-    
-  }
-
-  const filterResult = () => {
-    const fResult = Countries.filter((country) => 
+    const fResult = countries.filter((country) => 
       country.region === filter
     )
-     console.log("f-result", fResult)
-    return (
-      <div>
-        {
-          fResult.map((country, i) => {
-            return (
-              <div className="country-container" key={i}>
-                <img src={country.flags.svg}></img>
-                <span>
-                  <p>{country.name}</p>
-                  <p>Population: {country.population}</p>
-                  <p>Region: {country.region}</p>
-                  <p>Capital: {country.capital}</p>
-                </span>
-              </div>
-            );
-          })
-        }
-      </div> 
-    )
+    console.log("f-result", fResult)
+    
+    if (searchType === "search") {
+      return (
+        <div>
+          {
+            sResults.map((country, i) => {
+              return (
+                <div className="country-container" key={i}>
+                  <img src={country.flags.svg}></img>
+                  <span>
+                    <p>{country.name}</p>
+                    <p>Population: {country.population}</p>
+                    <p>Region: {country.region}</p>
+                    <p>Capital: {country.capital}</p>
+                  </span>
+                </div>
+              );
+            })
+          }
+        </div> 
+      );
+    } 
+    if (searchType === "filter") {
+      return (
+        <div>
+          {
+            fResult.map((country, i) => {
+              return (
+                <div className="country-container" key={i}>
+                  <img src={country.flags.svg}></img>
+                  <span>
+                    <p>{country.name}</p>
+                    <p>Population: {country.population}</p>
+                    <p>Region: {country.region}</p>
+                    <p>Capital: {country.capital}</p>
+                  </span>
+                </div>
+              );
+            })
+          }
+        </div> 
+      )
+    }
   }
+
+  
 
   return (
     <div>
-      { searchType === "search" || filter === "default" ? searchResults() : filterResult()}
+      {!searchType ? (
+          countries.map((country, i) => {
+            return (
+              <div className="country-container" key={i}>
+                <img alt="flag" title={country.name} src={country.flags.svg} />
+                <span>
+                  <p>{country.name}</p>
+                  <p>Population: {country.population}</p>
+                  <p>Region: {country.region}</p>
+                  <p>Capital: {country.capital}</p>
+                </span>
+              </div>
+            );
+          })
+        ) :  searchResults()
+      }
     </div>
   );
 }

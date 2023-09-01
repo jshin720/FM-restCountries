1cd2279d7b55dc18370d5d5bc0e2cb967c11a2b3
jshin720 <jeffshin720@gmail.com>
2023-09-01 17:45:29 -0400

    fixing bug in the search component

--- src/components/search/Search.jsx
+++ src/components/search/Search.jsx
@@ -24,10 +24,10 @@ function Search({keyword, filter, searchType}) {
                 <div className="country-container" key={i}>
                   <img src={country.flags.svg}></img>
                   <span>
-                    {/* <p>{country.name}</p>
+                    <p>{country.name}</p>
                     <p>Population: {country.population}</p>
                     <p>Region: {country.region}</p>
-                    <p>Capital: {country.capital}</p> */}
+                    <p>Capital: {country.capital}</p>
                   </span>
                 </div>
               );

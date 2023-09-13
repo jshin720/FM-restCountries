5af1d979cc05ebebc5a56fbeac93d10389ad6784
jshin720 <jeffshin720@gmail.com>
2023-09-13 15:26:47 -0400

    refactoring search  component

--- src/components/search/Search.jsx
+++ src/components/search/Search.jsx
@@ -66,14 +66,14 @@ function Search({keyword, filter, searchType}) {
       {!searchType ? (
           countries.map((country, i) => {
             return (
-              // <div className="country-container" key={i}>
-              //   <img alt="flag" title={country.name} src={country.flags.svg} />
-              //   <span>
-              //     <p>{country.name}</p>
-              //     <p>Population: {country.population}</p>
-              //     <p>Region: {country.region}</p>
-              //     <p>Capital: {country.capital}</p>
-              //   </span>
+              <div className="country-container" key={i}>
+                <img alt="flag" title={country.name} src={country.flags.svg} />
+                <span>
+                  <p>{country.name}</p>
+                  <p>Population: {country.population}</p>
+                  <p>Region: {country.region}</p>
+                  <p>Capital: {country.capital}</p>
+                </span>
               </div>
             );
           })

d73dd6c031632dcee0313fa862e226a2b0015b58
jshin720 <jeffshin720@gmail.com>
2023-09-15 12:08:30 -0400

    refactoring logic for search componentt

--- src/components/search/Search.jsx
+++ src/components/search/Search.jsx
@@ -7,35 +7,35 @@ function Search({keyword, filter, searchType}) {
   console.log("search component",keyword, filter, searchType)
 
   const searchResults = () => {
-    console.log('results', keyword)
-    console.log("f-result", filter)
+    // console.log('results', keyword)
+    // console.log("f-result", filter)
 
     const sResults = countries.filter((country) => country.name.toLowerCase().includes(keyword.toLowerCase()));
     const fResult = countries.filter((country) => 
       country.region === filter
     )
     
-    // if (searchType === "search") {
-    //   return (
-    //     <div>
-    //       {
-    //         sResults.map((country, i) => {
-    //           return (
-    //             <div className="country-container" key={i}>
-    //               <img src={country.flags.svg}></img>
-    //               <span>
-    //                 <p>{country.name}</p>
-    //                 <p>Population: {country.population}</p>
-    //                 <p>Region: {country.region}</p>
-    //                 <p>Capital: {country.capital}</p>
-    //               </span>
-    //             </div>
-    //           );
-    //         })
-    //       }
-    //     </div> 
-    //   );
-    // } 
+    if (searchType === "search") {
+      return (
+        <div>
+          {
+            sResults.map((country, i) => {
+              return (
+                <div className="country-container" key={i}>
+                  <img src={country.flags.svg}></img>
+                  <span>
+                    <p>{country.name}</p>
+                    <p>Population: {country.population}</p>
+                    <p>Region: {country.region}</p>
+                    <p>Capital: {country.capital}</p>
+                  </span>
+                </div>
+              );
+            })
+          }
+        </div> 
+      );
+    } 
     if (searchType === "filter") {
       return (
         <div>

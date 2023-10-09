f5d56bd44da16ab549aa98e663b2256775067fc9
jshin720 <jeffshin720@gmail.com>
2023-10-09 12:58:14 -0400

    fixed flag img not rendering

--- src/components/country/country.jsx
+++ src/components/country/country.jsx
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from "react";
 // import countries from "../../data.json";
 const url = "https://restcountries.com/v3.1/all";
 
@@ -6,42 +6,42 @@ function Countries() {
   const [countries, setCountries] = useState([]);
 
   const fetchCountryData = async () => {
-    const res = await fetch(url)
-    const countries = await res.json()
-    setCountries(countries)
+    const res = await fetch(url);
+    const countries = await res.json();
+    setCountries(countries);
     // console.log("count", countries);
-    }
+  };
 
   useEffect(() => {
     fetchCountryData();
   }, []);
 
-
-
-
-
   return (
     <div>
+      {countries.map((country, i) => {
+        const { flags, name } = country;
+        // console.log(country)
 
-    { countries.map((country, i) => {
-      const { flags } = country
-      // console.log(country)
-
-      return (
-        <article key={i}>
-         
-          <div>
-            <img src={flags.svg} alt={country.name.common} />
-            <h3>{country.name.common}</h3>
-            <h4>Population: {country.population}</h4>
-            <h4>Region: {country.region}</h4>
-            <h4>Capital: {country.capital}</h4>
-          </div>
-        </article>
-      );
-    })
-
-    }
+        return (
+          <article key={i}>
+            <div>
+              <img src={flags.svg} alt={name.common} />
+              <h3>
+                <span>{name.common}</span>
+              </h3>
+              <h4>
+                Population: <span>{country.population}</span>
+              </h4>
+              <h4>
+                Region: <span>{country.region}</span>
+              </h4>
+              <h4>
+                Capital: <span>{country.capital}</span>
+              </h4>
+            </div>
+          </article>
+        );
+      })}
       {/* <button className='back-button'>back</button>
       <div>
         <div className="img-container">

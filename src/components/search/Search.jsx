25caf8572b19b8d6f66e6301b53670e0884b4ea1
jshin720 <jeffshin720@gmail.com>
2023-11-15 11:16:46 -0500

    working on css for filter component

--- src/components/search/Search.jsx
+++ src/components/search/Search.jsx
@@ -63,24 +63,24 @@ function Search({keyword, filter, searchType}) {
   
 
   return (
-    <div>
-      {!searchType ? (
-          countries.map((country, i) => {
-            return (
-              <div className="country-container" key={i}>
-                <img alt="flag" title={country.name} src={country.flags.svg} />
-                <span>
-                  <p>{country.name}</p>
-                  <p>Population: {country.population}</p>
-                  {/* <p>Region: {country.region}</p> */}
-                  <p>Capital: {country.capital}</p>
-                </span>
-              </div>
-            );
-          })
-        ) :  searchResults()
-      }
-    </div>
+    // <div>
+    //   {!searchType ? (
+    //       countries.map((country, i) => {
+    //         return (
+    //           <div className="country-container" key={i}>
+    //             <img alt="flag" title={country.name} src={country.flags.svg} />
+    //             <span>
+    //               <p>{country.name}</p>
+    //               <p>Population: {country.population}</p>
+    //               {/* <p>Region: {country.region}</p> */}
+    //               <p>Capital: {country.capital}</p>
+    //             </span>
+    //           </div>
+    //         );
+    //       })
+    //     ) :  searchResults()
+    //   }
+    // </div>
 
     <section className="search-filter">
       <form>

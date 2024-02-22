eccfe2fea346367ab22f9a89b261e136b808796b
JShin720 <jeffshin720@gmail.com>
2024-02-22 13:44:05 -0500

    creating form for search component

--- src/components/search/Search.jsx
+++ src/components/search/Search.jsx
@@ -67,6 +67,16 @@ function Search({keyword, filter, searchType}) {
       <form action="" className="form-control">
         <input type="search" name="search" value={query} id="search" placeholder='Search for a Country' onChange={handleInput}/>
       </form>
+      <div>
+        <select name="" id="" className="country-select">
+          <option value="Filter by Region">Filter by Region</option>
+          <option value="Africa">Africa</option>
+          <option value="America">America</option>
+          <option value="Asia">Asia</option>
+          <option value="Europe">Europe</option>
+          <option value="Oceania">Oceania</option>
+        </select>
+      </div>
     </section>
   );
 }

ab0ed98626fd6958ddc65fac2c7ccf510b8572dd
JShin720 <jeffshin720@gmail.com>
2024-03-01 14:19:25 -0500

    working on country component

--- src/components/country/Country.jsx
+++ src/components/country/Country.jsx
@@ -4,6 +4,7 @@ import { Link } from 'react-router-dom';
 const Country = () => {
   return (
     <div>
+      <Link to="/">Back to Home</Link>
       <h1>individual country information</h1>
     </div>
   );

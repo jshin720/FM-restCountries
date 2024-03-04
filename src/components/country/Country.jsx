2c491582882c28731a81dafebf6ca3651f36620f
JShin720 <jeffshin720@gmail.com>
2024-03-04 13:26:07 -0500

    adding useState for country component

--- src/components/country/Country.jsx
+++ src/components/country/Country.jsx
@@ -2,6 +2,9 @@ import React, {useState} from 'react';
 import { Link } from 'react-router-dom';
 
 const Country = () => {
+  const [country, setCountry] = useState([])
+
+  
   return (
     <div>
       <Link to="/" className='btn btn-light'>Back to Home</Link>

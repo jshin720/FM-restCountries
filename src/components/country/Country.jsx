d28fc9eb12b521620fb30449055083f77e2be449
JShin720 <jeffshin720@gmail.com>
2024-03-04 13:26:39 -0500

    adding useEffect for country component

--- src/components/country/Country.jsx
+++ src/components/country/Country.jsx
@@ -1,10 +1,12 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import { Link } from 'react-router-dom';
 
 const Country = () => {
-  const [country, setCountry] = useState([])
+  const [country, setCountry] = useState([]);
 
-  
+
+
+
   return (
     <div>
       <Link to="/" className='btn btn-light'>Back to Home</Link>

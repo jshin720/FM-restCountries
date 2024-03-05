f56444e94587fe4b9f742e722585d375cc0f8989
JShin720 <jeffshin720@gmail.com>
2024-03-05 12:16:35 -0500

    adding useParams to intropolate country names

--- src/components/country/Country.jsx
+++ src/components/country/Country.jsx
@@ -1,28 +1,46 @@
-import React, {useState, useEffect} from 'react';
-import { Link, useParams } from 'react-router-dom';
+import React, { useState, useEffect } from "react";
+import { Link, useParams } from "react-router-dom";
 
 const Country = () => {
   const [country, setCountry] = useState([]);
-  const {name} = useParams()
+  const { name } = useParams();
 
-
-useEffect(() => {
-  const fetchCountry = async() => {
-    const res = await fetch(`https://restcountries.com/v3.1/name/${name}`
-    );
-    const country = await res.json();
-    console.log(country)
-    setCountry([...country]); 
-  }
-  fetchCountry();
-}, [])
+  useEffect(() => {
+    const fetchCountry = async () => {
+      const res = await fetch(`https://restcountries.com/v3.1/name/${name}`);
+      const country = await res.json();
+      setCountry([...country]);
+      console.log(country);
+    };
+    fetchCountry();
+  }, []);
 
   return (
     <div>
-      <Link to="/" className='btn btn-light'>Back to Home</Link>
+      <Link to="/" className="btn btn-light">
+        Back to Home
+      </Link>
       <h1>individual country information</h1>
+      <section className="country">
+        {country.map((c) => {
+          const {
+            id,
+            flag,
+            name,
+            nativeName,
+            population,
+            region,
+            subregion,
+            capital,
+            topLevelDomain,
+            currencies,
+            languages,
+            border,
+          } = c;
+        })}
+      </section>
     </div>
   );
-}
+};
 
 export default Country;

7d0b5d88fbc09164fe83de5643ce3bc0bf451189
JShin720 <jeffshin720@gmail.com>
2024-04-09 15:30:47 -0400

    fixing bugs in search component

--- src/components/search/Search.jsx
+++ src/components/search/Search.jsx
@@ -18,7 +18,7 @@ function Search({ keyword, filter, searchType }) {
         if (cName.innerText.toLowerCase().includes(value.toLowerCase())) {
           cName.parentElement.style.display = "block";
         } else {
-          // cName.parentElement.style.display = "none";
+          cName.parentElement.style.display = "none";
         }
       });
     });

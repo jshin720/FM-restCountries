602fa8c74785093aaf35c1eda031cad74999bef5
JShin720 <jeffshin720@gmail.com>
2024-04-26 16:46:22 -0400

    working on css

--- src/components/search/Search.jsx
+++ src/components/search/Search.jsx
@@ -24,7 +24,7 @@ function Search({ keyword, filter, searchType }) {
           cName.classList.add("hide")
         }
         } else {
-          // cName.parentElement.style.display = "none";
+          cName.parentElement.style.display = "none";
         }
       });
     });

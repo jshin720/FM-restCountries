f3a1ec78d4e195021ef10359a97e0cb3095ba80a
JShin720 <jeffshin720@gmail.com>
2024-05-14 14:34:28 -0400

    fixing bugs

--- src/components/countries/Countries.jsx
+++ src/components/countries/Countries.jsx
@@ -40,6 +40,14 @@ function Countries() {
             <Link to={`/countries/${country.id}`}>
               <img src={country.flags.png} alt={country.name.common} />
             </Link>
+            <div className="country-info">
+              <h3>{country.name.common}</h3>
+              <ul>  
+                <li>
+                  <strong>Native Name: </strong>
+                </li>
+              </ul>
+            </div>
       </div>
   }
   // console.log("aftersort", countries)

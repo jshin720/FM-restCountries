77089019d271012de35c033e1a47902220788477
JShin720 <jeffshin720@gmail.com>
2024-05-17 13:59:59 -0400

    fixing bugs

--- src/components/countries/Countries.jsx
+++ src/components/countries/Countries.jsx
@@ -42,7 +42,7 @@ function Countries() {
   return (
     <section className="country-grid">
       {countries.map((country, i) => {
-        // const { flags, name } = country;
+        const { flags, name } = country;
         console.log(country);
 
 
